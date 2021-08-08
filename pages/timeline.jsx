0633632aa3ab26064383b3c00d4ff1471807e79c
Jonathan García <megabyte9703@gmail.com>
2021-08-07 21:51:14 -0600

    Estilos globales

--- pages/timeline.jsx
+++ pages/timeline.jsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
+import AppLayout from "../components/AppLayout";
 
 const Timeline = () => (
   <>
-    <h1>This is the Timeline</h1>
-    <Link href="/">
-      <a>Go home</a>
-    </Link>
-    <style jsx>{`
-      h1 {
-        color: red;
-        font-size: 36px;
-      }
-    `}</style>
+    <AppLayout>
+      <h1>This is the Timeline</h1>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+      <style jsx>{`
+        h1 {
+          color: red;
+          font-size: 36px;
+        }
+      `}</style>
+    </AppLayout>
   </>
 );
 
