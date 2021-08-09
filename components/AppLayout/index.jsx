87e4b8ad97fa6e0daf1e922acce754ea0a119ab8
Jonathan García <megabyte9703@gmail.com>
2021-08-08 23:18:23 -0600

    Breakpoints y extraer estilos

--- components/AppLayout/index.jsx
+++ components/AppLayout/index.jsx
@@ -1,36 +1,14 @@
-import { fonts, colors } from "../../styles/theme";
-import { addOpacityColor } from "../../styles/utils";
-
-const backgroundColor = addOpacityColor(colors.primary, 0.3);
-console.log(backgroundColor);
+import styles, { global } from "./styles";
 const AppLayout = ({ children }) => {
   return (
     <>
-      <main>{children}</main>
-      <style jsx global>{`
-        html,
-        body {
-          background-image: radial-gradient(
-              ${backgroundColor} 1px,
-              transparent 1px
-            ),
-            radial-gradient(${backgroundColor} 1px, transparent 1px);
-          background-position: 0 0 25px 25px;
-          background-size: 50px 50px;
-          padding: 0;
-          margin: 0;
-          font-family: ${fonts.base};
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <div>
+        <main>{children}</main>
+      </div>
+      <style jsx>{styles}</style>
+      <style jsx global>
+        {global}
+      </style>
       ;
     </>
   );
---
+++ components/AppLayout/styles.js
@@ -0,0 +1,49 @@
+import css from "styled-jsx/css";
+import { fonts, colors, breakpoints } from "../../styles/theme";
+import { addOpacityColor } from "../../styles/utils";
+
+const backgroundColor = addOpacityColor(colors.primary, 0.3);
+
+export const global = css.global`
+  html,
+  body {
+    background-image: radial-gradient(${backgroundColor} 1px, transparent 1px),
+      radial-gradient(${backgroundColor} 1px, transparent 1px);
+    background-position: 0 0 25px 25px;
+    background-size: 50px 50px;
+    padding: 0;
+    margin: 0;
+    font-family: ${fonts.base};
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default css`
+  div {
+    display: grid;
+    height: 100vh;
+    place-items: center;
+  }
+  main {
+    background: #fff;
+    border-radius: 10px;
+    box-shadow: 0 10px 25px rgba(0, 0, 0, 0.3);
+    height: 100%;
+    width: 100%;
+  }
+
+  @media (min-width: ${breakpoints.mobile}) {
+    main {
+      width: ${breakpoints.mobile};
+      height: 90vh;
+    }
+  }
+`;
