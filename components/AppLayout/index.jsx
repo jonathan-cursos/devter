501aab6db3718791f03509020199c557772ec0c8
Jonathan García <megabyte9703@gmail.com>
2021-08-08 22:19:34 -0600

    Utilidad para opacidad

--- components/AppLayout/index.jsx
+++ components/AppLayout/index.jsx
@@ -1,4 +1,8 @@
-import { fonts } from "../../styles/theme";
+import { fonts, colors } from "../../styles/theme";
+import { addOpacityColor } from "../../styles/utils";
+
+const backgroundColor = addOpacityColor(colors.primary, 0.3);
+console.log(backgroundColor);
 const AppLayout = ({ children }) => {
   return (
     <>
@@ -6,6 +10,13 @@ const AppLayout = ({ children }) => {
       <style jsx global>{`
         html,
         body {
+          background-image: radial-gradient(
+              ${backgroundColor} 1px,
+              transparent 1px
+            ),
+            radial-gradient(${backgroundColor} 1px, transparent 1px);
+          background-position: 0 0 25px 25px;
+          background-size: 50px 50px;
           padding: 0;
           margin: 0;
           font-family: ${fonts.base};
--- styles/theme.js
+++ styles/theme.js
@@ -1,3 +1,8 @@
 export const fonts = {
   base: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
 };
+
+export const colors = {
+  primary: "#0099ff",
+  secondary: "#1c5480",
+};
