a85c151823233b762e41a56f9c994ae33502b327
Jonathan García <jona03g97@gmail.com>
2021-08-13 21:46:15 -0600

    Endpoints y peticiones a la API

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,21 +1,44 @@
 import AppLayout from 'components/AppLayout'
+import Devit from 'components/Devit'
+import { useEffect, useState } from 'react'
 
 export default function HomePage() {
+  const [timeLine, setTimeLine] = useState([])
+
+  useEffect(() => {
+    fetch('/api/statuses/home_timeline')
+      .then((res) => res.json())
+      .then(setTimeLine)
+  }, [])
   return (
     <>
       <AppLayout>
         <header>
           <h2>Inicio</h2>
         </header>
-        <section></section>
+        <section>
+          {timeLine.map(({ id, username, avatar, message }) => {
+            return (
+              <Devit
+                key={id}
+                username={username}
+                avatar={avatar}
+                message={message}
+                id={id}
+              />
+            )
+          })}
+        </section>
         <nav></nav>
       </AppLayout>
       <style jsx>{`
         header {
-          height: 49px;
-          position: fixed;
-          top: 0;
+          align-items: center;
           border-bottom: 1px solid #ccc;
+          height: 49px;
+          display: flex;
+          position: sticky;
+          top: 0;
           width: 100%;
         }
         h2 {
@@ -23,14 +46,14 @@ export default function HomePage() {
           font-size: 21px;
         }
         nav {
+          bottom: 0;
+          border-top: 1px solid #ccc;
           height: 49px;
-          position: fixed;
-          top: 0;
+          position: sticky;
           width: 100%;
-          border-bottom: 1px solid #ccc;
         }
         section {
-          padding-top: 100px;
+          padding-top: 49px;
         }
       `}</style>
     </>
