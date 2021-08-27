40bfc3024470afaec7ecdf2984a26430d9fa453c
Jonathan García <megabyte9703@gmail.com>
2021-08-27 01:19:44 -0600

    Añadir fondo redondo al hover

--- components/AppLayout/styles.js
+++ components/AppLayout/styles.js
@@ -46,6 +46,8 @@ export default css`
     width: 100%;
     position: relative;
     overflow-y: auto;
+    display: flex;
+    flex-direction: column;
   }
 
   @media (min-width: ${breakpoints.mobile}) {
--- pages/home/index.js
+++ pages/home/index.js
@@ -3,6 +3,11 @@ import Devit from 'components/Devit'
 import { fetchLatestDevits } from 'firebase/client'
 import useUser from 'hooks/useUser'
 import { useEffect, useState } from 'react'
+import Create from 'components/Icons/Create'
+import Home from 'components/Icons/Home'
+import Search from 'components/Icons/Search'
+import Link from 'next/link'
+import { colors } from 'styles/theme'
 
 export default function HomePage() {
   const [timeLine, setTimeLine] = useState([])
@@ -35,7 +40,23 @@ export default function HomePage() {
             }
           )}
         </section>
-        <nav></nav>
+        <nav>
+          <Link href='/'>
+            <a>
+              <Home stroke='#09f' width={32} height={32} />
+            </a>
+          </Link>
+          <Link href='/compose/tweet'>
+            <a>
+              <Search stroke='#09f' width={32} height={32} />
+            </a>
+          </Link>
+          <Link href='/compose/tweet'>
+            <a>
+              <Create stroke='#09f' width={32} height={32} />
+            </a>
+          </Link>
+        </nav>
       </AppLayout>
       <style jsx>{`
         header {
@@ -49,6 +70,10 @@ export default function HomePage() {
           top: 0;
           width: 100%;
         }
+
+        section {
+          flex: 1;
+        }
         h2 {
           font-weight: 800;
           font-size: 21px;
@@ -61,6 +86,25 @@ export default function HomePage() {
           position: sticky;
           width: 100%;
           background: #fff;
+          display: flex;
+        }
+
+        nav a {
+          display: flex;
+          align-items: center;
+          flex: 1 1 auto;
+          height: 100%;
+          justify-content: center;
+        }
+
+        nav a:hover {
+          background: radial-gradient(#0099ff22 15%, transparent 16%);
+          background-size: 180px 180px;
+          background-position: center;
+        }
+
+        nav a:hover > :global(svg) {
+          stroke: ${colors.primary};
         }
       `}</style>
     </>
