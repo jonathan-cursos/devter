95c51fcfb7a429314154f0aec9a6fbd29d517e6b
Jonathan García <megabyte9703@gmail.com>
2021-08-28 11:20:38 -0600

    Hover a un elemento con otros elementos clickeables dentro

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,5 +1,7 @@
 import Avatar from 'components/Avatar'
 import useTimeAgo from 'hooks/useTimeAgo'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Devit({
   avatar,
@@ -10,9 +12,15 @@ export default function Devit({
   img
 }) {
   const timeAgo = useTimeAgo(createdAt)
+  const router = useRouter()
+
+  const handleArticleClick = (e) => {
+    e.preventDefault()
+    router.push(`/status/${id}`)
+  }
   return (
     <>
-      <article>
+      <article onClick={handleArticleClick}>
         <div>
           <Avatar alt={userName} src={avatar} />
         </div>
@@ -20,7 +28,11 @@ export default function Devit({
           <header>
             <strong>{userName}</strong>
             <span> . </span>
-            <time>{timeAgo}</time>
+            <Link href={`/status/${id}`}>
+              <a>
+                <time>{timeAgo}</time>
+              </a>
+            </Link>
           </header>
           <p>{content}</p>
           {img && <img src={img} />}
@@ -31,6 +43,11 @@ export default function Devit({
           padding: 10px 15px;
           display: flex;
           border-bottom: 2px solid #eee;
+        }
+
+        article:hover {
+          background: #f5f8fa;
+          cursor: pointer;
         }
 
         img {
@@ -52,6 +69,16 @@ export default function Devit({
         time {
           color: #555;
           font-size: 14px;
+        }
+
+        a {
+          color: #555;
+          font-size: 14px;
+          text-decoration: none;
+        }
+
+        a:hover {
+          text-decoration: underline;
         }
       `}</style>
     </>
