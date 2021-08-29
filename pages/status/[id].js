266eb62006e4042cd9fbcaa4780c791011d189bf
Jonathan García <megabyte9703@gmail.com>
2021-08-29 08:49:48 -0600

    getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -9,14 +9,28 @@ export default function DevitPage(props) {
   )
 }
 
-DevitPage.getInitialProps = (ctx) => {
-  const { query, res } = ctx
-  const { id } = query
+export async function getServerSideProps(ctx) {
+  const { params, res } = ctx
+  const { id } = params
 
-  return fetch(`http://localhost:3000/api/devits/${id}`).then((apiRes) => {
-    if (apiRes.ok) return apiRes.json()
-    if (res) {
-      res.writeHead(301, { Location: '/home' }).end()
-    }
-  })
+  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return { props }
+  }
+  if (res) {
+    res.writeHead(301, { Location: '/home' }).end()
+  }
 }
+
+// DevitPage.getInitialProps = (ctx) => {
+//   const { query, res } = ctx
+//   const { id } = query
+
+//   return fetch(`http://localhost:3000/api/devits/${id}`).then((apiRes) => {
+//     if (apiRes.ok) return apiRes.json()
+//     if (res) {
+//       res.writeHead(301, { Location: '/home' }).end()
+//     }
+//   })
+// }
