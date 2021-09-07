70c9e3cabe2cf8857b72b0066100f5b610d25932
Jonathan García <megabyte9703@gmail.com>
2021-09-06 20:51:09 -0600

    Mejoras progresivas

--- hooks/useDateTimeFormat.js
+++ hooks/useDateTimeFormat.js
@@ -1,9 +1,23 @@
+// Esto nos permite que, al hacer un hover sobre la fecha, nos muestre una tarjeta con esta fecha
+// En nuestro caso no funciona, porque no traemos el cambio hecho por el estudiante, solo este hook
+
 import { DEFAULT_LANGUAGE } from 'constants/locale'
 
-export default function useDateTimeFormat(timestamp) {
-  const date = new Date(timestamp)
-  const language = DEFAULT_LANGUAGE
+const isDateTimeFormatSupported =
+  typeof Intl !== 'undefined' && Intl.DateTimeFormat
 
+export const formDate = (timestamp, { language = DEFAULT_LANGUAGE }) => {
+  const date = new Date(timestamp)
+
+  if (!isDateTimeFormatSupported) {
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    }
+    return date.toLocaleDateString(language, options)
+  }
   const options = {
     year: 'numeric',
     month: 'numeric',
@@ -14,4 +28,8 @@ export default function useDateTimeFormat(timestamp) {
   }
 
   return new Intl.DateTimeFormat(language, options).format(date)
+}
+
+export default function useDateTimeFormat(timestamp) {
+  return formDate(timestamp, { language: DEFAULT_LANGUAGE })
 }
