9f61e61c1201f77addc4c13d54e51df4ee6a126e
Jonathan García <megabyte9703@gmail.com>
2021-11-09 17:27:25 -0600

    Variables de entorno

--- firebase/client.js
+++ firebase/client.js
@@ -1,15 +1,17 @@
 import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDBtisc4MF1XicKQKaABskIxvoBcN0-dmM',
-  authDomain: 'devter-35777.firebaseapp.com',
-  projectId: 'devter-35777',
-  storageBucket: 'devter-35777.appspot.com',
-  messagingSenderId: '1067685632479',
-  appId: '1:1067685632479:web:a3440f1ece40489285cc6a',
-  measurementId: 'G-759D9QTPJR'
-}
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDBtisc4MF1XicKQKaABskIxvoBcN0-dmM',
+//   authDomain: 'devter-35777.firebaseapp.com',
+//   projectId: 'devter-35777',
+//   storageBucket: 'devter-35777.appspot.com',
+//   messagingSenderId: '1067685632479',
+//   appId: '1:1067685632479:web:a3440f1ece40489285cc6a',
+//   measurementId: 'G-759D9QTPJR'
+// }
+
+const firebaseConfig = JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_CONFIG)
 
 // siempre que recargamos con el autorefresh, sí ya teniamos una app incializada
 // dara error, debemos inicializarla solo sí no lo está
